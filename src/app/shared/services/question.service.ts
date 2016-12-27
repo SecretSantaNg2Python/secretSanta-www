b75c0a7e643c018a35ad91dd75cd451cc7e0cda0
nathan-j-brenner <nathan.j.brenner@gmail.com>
2016-12-27 14:14:32 -0800

    brought back in validation password field

--- src/app/shared/services/question.service.ts
+++ src/app/shared/services/question.service.ts
@@ -9,31 +9,32 @@ export class QuestionService {
   // Todo: make asynchronous
   getRegistrationQuestions() {
     let questions: QuestionBase<any>[] = [
-	  new TextboxQuestion({
-   	   	key: 'username',
-   	   	label: 'Username',
+      new TextboxQuestion({
+        key: 'username',
+        label: 'Username',
    	   	required: true,
-   	   	order: 1
-   	   }),
+        order: 1
+   	  }),
       new TextboxQuestion({
         key: 'email',
         label: 'Email',
         required: true,
-        order: 1
+        order: 2
       }),
+      // todo: add custom validation that checks these two fields
+      // currently, the api will just return an error response if they don't match
       new TextboxQuestion({
         key: 'password',
-		label: 'Password',
-		required: true,
-        order: 2
-	})
-		// Dynamic input, error message for non-matching passwords and block save
-		// new TextboxQuestion({
-		//   key: 'verify_password',
-		//   label: 'Verify Password',
-		//   required: true,
-		//   order: 2
-		// })
+        label: 'Password',
+        required: true,
+        order: 3
+      }),
+      new TextboxQuestion({
+        key: 'verify_password',
+        label: 'Verify Password',
+        required: true,
+        order: 4
+      })
     ];
     return questions.sort((a, b) => a.order - b.order);
   }
@@ -42,16 +43,16 @@ export class QuestionService {
     let questions: QuestionBase<any>[] = [
       new TextboxQuestion({
         key: 'email',
-		label: 'Email',
+        label: 'Email',
         required: true,
         order: 1
       }),
       new TextboxQuestion({
         key: 'password',
-		label: 'Password',
-		required: true,
+        label: 'Password',
+        required: true,
         order: 2
-	  })
+      })
     ];
     return questions.sort((a, b) => a.order - b.order);
   }

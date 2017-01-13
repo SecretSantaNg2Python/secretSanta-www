17747298f3abe4052c1e7ab9d1b8feb9345c7223
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-13 15:30:55 -0800

    reorganized and added effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+// angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,24 +6,39 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
+
+// ngrx
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
+// root component
 import { AppComponent } from './app.component';
-import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
-import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+
+// routing
+import { AppRoutingModule } from './app.routing';
+
+// container components
+import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-import { AppRoutingModule } from './app.routing';
-import { HomeComponent } from './home/home.component';
+// child components
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+// services
 import { QuestionService } from './shared/services/question.service';
 import { CommonService } from './shared/services/common.service';
+import { SessionService } from './shared/services/session.service';
 
+// reducers
 import { sessionReducer } from './shared/reducers/session.reducer';
+
+// effects
+import { SessionEffects } from './shared/effects/session.effects';
 
 @NgModule({
   declarations: [
@@ -36,6 +52,7 @@ import { sessionReducer } from './shared/reducers/session.reducer';
     HomeComponent
   ],
   imports: [
+    EffectsModule.run(SessionEffects),
     StoreModule.provideStore({
       session: sessionReducer
     }),
@@ -50,6 +67,7 @@ import { sessionReducer } from './shared/reducers/session.reducer';
   providers: [
     CommonService,
     QuestionService,
+    SessionService
   ],
   bootstrap: [AppComponent]
 })

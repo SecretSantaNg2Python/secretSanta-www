62e83eb14adb45e08d89ebb7f81a7a67294c0743
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-14 13:27:49 -0800

    moved into a separate file

--- src/app/shared/services/common.service.ts
+++ src/app/shared/services/common.service.ts
@@ -4,33 +4,7 @@ import { Observable } from 'rxjs/Observable'
 
 import { API_ENDPOINT } from '../api';
 import { ERROR_ACTIONS } from '../reducers/error.reducer';
-
-interface DeleteParams {
-    uri: string, 
-    successActionType: string,
-    errorActionType: string
-}
-
-interface GetParams {
-  url: string,
-  auth: boolean,
-  successActionType: string,
-  errorActionType: string
-}
-
-interface PostParams {
-    uri: string, // the api endpoint
-    payload: {}, // what you're sending on the http request, should be data from a form
-    auth: boolean, // on true, then the token will be on the header
-    succesActionType: string, // If the http responce is a success, send this action type to the reducer, should be something like `ACTION_TYPES.ACTION_TYPE.SUCCESS`
-    responseObject: string, // this should be a property on the success resposonse body object
-    errorActionType: string, // the failure action, should look something like `ACTION_TYPES.ACTION_TYPE.FAILURE`
-}
-
-interface PutParams {
-  url: string, 
-  payload: {},
-}
+import { DeleteParams, GetParams, PostParams, PutParams } from '../models/httpParams.interface';
 
 @Injectable()
 export class CommonService {

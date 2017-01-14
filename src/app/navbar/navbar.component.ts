41404e40038aeb6b2e6efbb3771f6794db7c3b7c
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-14 13:55:35 -0800

    displaying dynamic data

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent implements OnInit {
+  @Input() user;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }

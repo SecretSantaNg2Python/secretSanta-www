eb50f62539185b466b1a397e4062fbd1c9b5b24b
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-14 16:15:36 -0800

    started on pesisting the session

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { Session } from './shared/models/session.interface';
@@ -14,12 +14,28 @@ import { Session } from './shared/models/session.interface';
   styleUrls: ['./app.component.css'],
   providers:  []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   user;
 
+  checkLocalStorage(){
+    if (localStorage['Authorization']) return this.getUser();
+  }
+
   constructor(private _store: Store<any>){
     _store.select('session').subscribe((session: Session) => this.user = session.user);
+  }
+
+  getUser(){
+    // need to create this session action
+    // this._store.dispatch({
+    //   type: SESSION_ACTIONS.GET_USER.ATTEMPT,
+    //   payload: { }
+    // });
+  }
+
+  ngOnInit(){
+
   }
 
   onSignOut(){

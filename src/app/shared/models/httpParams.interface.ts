17a3fddd1118f9857e1bbb58c232b9592ee6f44e
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-15 13:37:33 -0800

    abstracted into a single interface

--- src/app/shared/models/httpParams.interface.ts
+++ src/app/shared/models/httpParams.interface.ts
@@ -1,52 +1,8 @@
-export interface DeleteParams {
-    uri: string, 
-    successActionType: string,
-    errorActionType: string
-}
-
-export interface GetParams {
-  url: string,
-  auth: boolean,
-  successActionType: string,
-  errorActionType: string
-}
-
-export interface PostParams {
-    uri: string, // the api endpoint
-    payload: {}, // what you're sending on the http request, should be data from a form
-    auth: boolean, // on true, then the token will be on the header
-    succesActionType: string, // If the http responce is a success, send this action type to the reducer, should be something like `ACTION_TYPES.ACTION_TYPE.SUCCESS`
-    responseObject: string, // this should be a property on the success resposonse body object
-    errorActionType: string, // the failure action, should look something like `ACTION_TYPES.ACTION_TYPE.FAILURE`
-}
-
-export interface PutParams {
-  url: string, 
-  payload: {},
-}
-
 export interface HttpParams {
-	DeleteParams: {
-		uri: string, 
-		successActionType: string,
-		errorActionType: string
-	},
-	GetParams: {
-		url: string,
-		auth: boolean,
-		successActionType: string,
-		errorActionType: string
-	},
-	PostParams: {
-		uri: string, // the api endpoint
-		payload: {}, // what you're sending on the http request, should be data from a form
-		auth: boolean, // on true, then the token will be on the header
-		succesActionType: string, // If the http responce is a success, send this action type to the reducer, should be something like `ACTION_TYPES.ACTION_TYPE.SUCCESS`
-		responseObject: string, // this should be a property on the success resposonse body object
-		errorActionType: string, // the failure action, should look something like `ACTION_TYPES.ACTION_TYPE.FAILURE`
-	},
-	PutParams: {
-		url: string, 
-		payload: {},
-	}
+	auth?: boolean, // on true, then the token will be on the header. Optional (not needed for all post and get requests)
+	errorActionType: string, // the failure action, should look something like `ACTION_TYPES.ACTION_TYPE.FAILURE`
+	payload?: {}, // what you're sending on the http request, should be data from a form. Optional: not needed for get and delete requests
+	responseObject: string, // this should be a property on the success resposonse body object
+	successActionType: string, // If the http responce is a success, send this action type to the reducer, should be something like `ACTION_TYPES.ACTION_TYPE.SUCCESS`
+	uri: string, // the api endpoint
 }

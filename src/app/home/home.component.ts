ce1879fffa3883001ae86c1d04bcc84e789d9f7b
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-14 16:52:20 -0800

    working with async pipe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable} from 'rxjs/Observable'
-
 import { Session } from '../shared/models/session.interface';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  public user;
+  public session;
   private subscription;
-
   constructor(private _store: Store<any>) {
-    // _store.select('session').subscribe((session: Session) => this.user = session.user);
-    this.subscription = this._store
-      .select('session')
-      .subscribe((session: Session) => {
-        this.user = session.user;
-    });
-
+    this.session = _store.select('session');
   }
-
   ngOnInit() {
   }
-
-  ngOnDestroy(){
-    this.subscription.unsubscribe();
-  }
-
 }

ff76c92e561e9ed8240425d7fd678f19e4631106
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-15 13:38:11 -0800

    added configRequest method

--- src/app/shared/services/common.service.ts
+++ src/app/shared/services/common.service.ts
@@ -4,83 +4,101 @@ import { Observable } from 'rxjs/Observable'
 
 import { API_ENDPOINT } from '../api';
 import { ERROR_ACTIONS } from '../reducers/error.reducer';
-import { DeleteParams, GetParams, PostParams, PutParams } from '../models/httpParams.interface';
+import { HttpParams } from '../models/httpParams.interface';
 
 @Injectable()
-export class CommonService {
-  // When you're looking to use this, start with the post method first.  The parameters for the rest of the methods are similar
+export class HttpWrapperService {
+  /*
+    These are the methods that are used in the additional services, 
+    where otherwise they would require importing angular 2 http module.
 
-  // A service should be created for each api endpoint, and should have a reducer, effects, and model
+    This keeps the services DRY, easier to test, and scalable.
+
+  */
 
   constructor(private http: Http) { }
 
-  delete(deleteParams: DeleteParams){
-    let apiUrl = `${API_ENDPOINT}/${deleteParams.uri}`;
-    let headers = new Headers({'Content-Type': 'application/json', 'Authorization': `token ${localStorage['Authorization']}`});
+  private configRequest(uri: string, authRequired: boolean = false): {apiUrl: string, options: RequestOptions} {
+    let apiUrl = `${API_ENDPOINT()}/${uri}`;
+
+    let headers = authRequired ? 
+      new Headers({
+        'Content-Type': 'application/json',
+        'Authorization' : `token ${localStorage['Authorization']}`
+      }) : 
+      new Headers({'Content-Type': 'application/json'});
     let options = new RequestOptions({ headers: headers });
+
+    return {apiUrl, options};
+  }
+
+  public delete(params: HttpParams){
+    let {apiUrl, options} = this.configRequest(params.uri, true)
 
     return this.http.delete(apiUrl, options)
       .map(res => ({
-        type: deleteParams.successActionType,
+        type: params.successActionType,
         payload: res.json()
       }))
       .catch(res => Observable.of({
         type: ERROR_ACTIONS.REPORT_ERROR,
         payload: {
-          action_type: deleteParams.errorActionType,
+          action_type: params.errorActionType,
           message: res.json().error
         }
       }));
   }
 
-  get(getParams: GetParams){
-
-    let apiUrl = `${API_ENDPOINT}/${getParams.url}`;
-    let headers = getParams.auth ? new Headers({'Authorization': `token ${localStorage['Authorization']}`}) : new Headers({});
-    let options = new RequestOptions({ headers: headers });
+  public get(params: HttpParams){
+    let {apiUrl, options} = this.configRequest(params.uri, params.auth)
 
     return this.http.get(apiUrl, options)
       .map(res => ({
-        type: getParams.successActionType,
+        type: params.successActionType,
         payload: res.json().categories
       }))
       .catch(res => Observable.of({
         type: ERROR_ACTIONS.REPORT_ERROR,
         payload: {
-          action_type: getParams.errorActionType,
+          action_type: params.errorActionType,
           message: res.json()
         }
       }))
   }
 
-  post(postParams: PostParams){
-    let apiUrl = `${API_ENDPOINT}/${postParams.uri}`;
-    let headers = postParams.auth ? 
-      new Headers({'Content-Type': 'application/json', 'Authorization': `token ${localStorage['Authorization']}`})
-      :
-      new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
+  public post(params: HttpParams){
+
+    let {apiUrl, options} = this.configRequest(params.uri, params.auth)
     
-    return this.http.post(apiUrl, postParams.payload, options)
+    return this.http.post(apiUrl, params.payload, options)
       .map(res => ({
-        type: postParams.succesActionType,
-        payload: res.json()[postParams.responseObject]
+        type: params.successActionType,
+        payload: res.json()[params.responseObject]
       }))
       .catch(res => Observable.of({
         type: ERROR_ACTIONS.REPORT_ERROR,
         payload: {
-          action_type: postParams.errorActionType,
+          action_type: params.errorActionType,
           message: res.json().error
         }
       }));
   }
 
-  put(putParams: PutParams){
-    let apiUrl = `${API_ENDPOINT}/${putParams.url}`;
-    let headers = new Headers({'Authorization': `token ${localStorage['Authorization']}`});
-    let options = new RequestOptions({ headers: headers });
+  put(params: HttpParams){
+    let {apiUrl, options} = this.configRequest(params.uri, true)
 
-    return this.http.put(apiUrl, putParams.payload, options);
+    return this.http.put(apiUrl, params.payload, options)
+      .map(res => ({
+        type: params.successActionType,
+        payload: res.json()[params.responseObject]
+      }))
+      .catch(res => Observable.of({
+        type: ERROR_ACTIONS.REPORT_ERROR,
+        payload: {
+          action_type: params.errorActionType,
+          message: res.json().error
+        }
+      }));
   }
 
 }

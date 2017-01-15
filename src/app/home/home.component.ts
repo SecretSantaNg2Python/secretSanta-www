658cf24dc186c9e058c8085d5d91d8de8bbbc596
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-14 16:16:41 -0800

    unsubscribe on destroy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable} from 'rxjs/Observable'
 
 import { Session } from '../shared/models/session.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  user;
+  public user;
+  private subscription;
 
   constructor(private _store: Store<any>) {
-    _store.select('session').subscribe((session: Session) => this.user = session.user);
+    // _store.select('session').subscribe((session: Session) => this.user = session.user);
+    this.subscription = this._store
+      .select('session')
+      .subscribe((session: Session) => {
+        this.user = session.user;
+    });
+
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
